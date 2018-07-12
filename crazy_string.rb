51a9eca7434dfd169547dc5b1ad0153874ec8f82
Chamberlain Churchill <cbchurchill2004@icloud.com>
2018-07-12 21:31:20 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
 def crazy_strings(a,b)
-
+  a.reverse.upcase
+  b.swapcase.gsub("s", "z").gsub("S", "Z")
 end
+
+crazy_strings("ChaCha", "Chamberlain")
+crazy_strings("Janice", "Amanda")
