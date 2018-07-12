2ffa2e2ae25a31a10200836f0a4a8703aa965f8c
Chamberlain Churchill <cbchurchill2004@icloud.com>
2018-07-12 21:32:32 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -5,5 +5,5 @@ def crazy_strings(a,b)
   b.swapcase.gsub("s", "z").gsub("S", "Z")
 end
 
-crazy_strings("ChaCha", "Chamberlain")
-crazy_strings("Janice", "Amanda")
+puts crazy_strings("ChaCha", "Chamberlain")
+puts crazy_strings("Janice", "Amanda")
